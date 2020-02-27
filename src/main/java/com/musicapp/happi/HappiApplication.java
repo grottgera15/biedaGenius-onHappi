8eacb9c687cadee5e93cdcd6e3999c85853826f3
Vulpes <jeremi.lisek@gmail.com>
2020-02-27 12:36:15 +0100

    initial commit

---
+++ src/main/java/com/musicapp/happi/HappiApplication.java
@@ -0,0 +1,13 @@
+package com.musicapp.happi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musicapp/happi/HappiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicapp.happi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
