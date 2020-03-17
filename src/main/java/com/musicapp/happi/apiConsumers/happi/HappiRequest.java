3a4420d70e708a2d38455edb77211f905b0666e1
xVulpeSx <jeremi.lisek@gmail.com>
2020-03-17 13:17:50 +0100

    dbSeederChanged, progress on HappiConsumer

--- src/main/java/com/musicapp/happi/apiConsumers/happi/HappiRequest.java
+++ src/main/java/com/musicapp/happi/apiConsumers/happi/HappiRequest.java
@@ -2,24 +2,34 @@ package com.musicapp.happi.apiConsumers.happi;
 
 import com.musicapp.happi.apiConsumers.DataTransferObjectTemplate;
 import com.musicapp.happi.apiConsumers.RequestTemplate;
+import javafx.util.Pair;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HappiRequest implements RequestTemplate {
-    private String address;
+    private String address = new String("https://api.happi.dev/v1/music/");
     private List<String> pathParams;
-    private Map<String, String> apiParams
+    private Map<String, String> apiParams;
+    private Pair<String, String> apikey = new Pair<>("apikey", "4728a2ukk9fAUKeoYWXbYPiQQ6YfAJNb60xRXSIwt3BITlwycdg2rVxH");
 
+    public HappiRequest(List<String> pathParams, Map<String, String> apiParams) {
+        this.pathParams = pathParams;
+        this.apiParams = apiParams;
+    }
 
     @Override
-    public String requestUrlAddress() {
-
+    public String requestUrlAddress(){
+        RestTemplate restTemplate = new RestTemplate();
 
         return  null;
     }
--- src/main/java/com/musicapp/happi/dataBase/DbSeeder.java
+++ src/main/java/com/musicapp/happi/dataBase/DbSeeder.java
@@ -44,7 +44,7 @@ public class DbSeeder {
         Artist alek = new Artist(
                 "alekIdFromApi",
                 "Aleksander 'Dusza Artysty' Szamalek",
-                "C:/twojaStara.jpg",
+                "https://scontent-waw1-1.xx.fbcdn.net/v/t31.0-8/p960x960/15039647_1124748574281831_9178505682944436220_o.jpg?_nc_cat=101&_nc_sid=85a577&_nc_ohc=vHyAGyMNkGwAX-tUtKK&_nc_ht=scontent-waw1-1.xx&_nc_tp=6&oh=0a08ebfd533bdb42b799ddf926f1587c&oe=5E952BAE",
                 "happi/do/pappi/alek.xD",
                 "Alek zajebiscice gra na swojej kurwa Gitarce. i. c.h.u.j.",
                 "happi/alek'sAlbumsList",
@@ -52,7 +52,7 @@ public class DbSeeder {
                         new Album(
                                 "album#asdfasfaw",
                                 "ONA MA JUSZ MOKRO. MOOOOOODRDO",
-                                "C:/onaMaMokro.jp100%",
+                                "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/11156142_784314594991899_2874198081674526601_n.jpg?_nc_cat=101&_nc_sid=7aed08&_nc_ohc=ebEdQVwmsIMAX9ZY952&_nc_ht=scontent-waw1-1.xx&oh=2e02d6b844f80ac076548677256a26fe&oe=5E979802",
                                 "12/06/1999",
                                 "happi/onaMaMokro.xD",
                                 Arrays.asList(
@@ -74,7 +74,7 @@ public class DbSeeder {
                         new Album(
                                 "album#fdsagfwga",
                                 "Kupe sobie sczele z rana",
-                                "gunwo.jp100%",
+                                "https://scontent-waw1-1.xx.fbcdn.net/v/t31.0-8/11709922_818958814860810_28242477012168652_o.jpg?_nc_cat=100&_nc_sid=dd9801&_nc_ohc=yzeg0lHL5eIAX9ca4P9&_nc_ht=scontent-waw1-1.xx&oh=7364dccfb24ad6f014354172ced9640a&oe=5E979AE9",
                                 "12/06/2009",
                                 "happi/kupka.xD",
                                 Arrays.asList(
@@ -92,7 +92,7 @@ public class DbSeeder {
         Artist maks = new Artist(
                 "asdfahw342hasdf",
                 "Magz I chuj",
-                "C:/twojStary.jpg",
+                "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/p960x960/84829814_3509320042476802_6790332536082923520_o.jpg?_nc_cat=108&_nc_sid=dbb9e7&_nc_ohc=Y6IH2Rn974oAX89-M51&_nc_ht=scontent-waw1-1.xx&_nc_tp=6&oh=a9c8fee167096b4bc93659969360ffdf&oe=5E953550",
                 "happi/do/pappi/magz.xD",
                 "No siema witam w mojej kuchni, dzisiaj wam pokaze ze muzyka to tez jest jak gotowanie tylko ze jest wiecej matmy, zapraszam i. c.h.u.j.",
                 "happi/maksi'sAlbumsList",
@@ -100,7 +100,7 @@ public class DbSeeder {
                         new Album(
                                 "fewq#asdfasfaw",
                                 "Pierdole to hehehe.",
-                                "jebanie.jp100%",
+                                "https://scontent-waw1-1.xx.fbcdn.net/v/t31.0-0/p640x640/26240076_1656633317729613_3937414924389579923_o.jpg?_nc_cat=111&_nc_sid=e007fa&_nc_ohc=ilouKLJADJkAX9ebtWa&_nc_ht=scontent-waw1-1.xx&_nc_tp=6&oh=04d0f0227231c8a679376d9a1c96f664&oe=5E94904B",
                                 "12/09/1998",
                                 "happi/onaMaMfdasfew.xD",
                                 Arrays.asList(
@@ -122,7 +122,7 @@ public class DbSeeder {
                         new Album(
                                 "album#fdsagfwgasdwefasdfaw3tf",
                                 "Hanna&Karo",
-                                "bylaAObecna.jp100%",
+                                "https://scontent-waw1-1.xx.fbcdn.net/v/t31.0-8/p960x960/14976770_1258743520852637_1448091983873138492_o.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=CIgmmsULnEQAX_o39l1&_nc_ht=scontent-waw1-1.xx&_nc_tp=6&oh=c7be5d4cfe6771e85b9d75641c369ba1&oe=5E965B0E",
                                 "12/05/2009",
                                 "happi/ups.xD",
                                 Arrays.asList(
