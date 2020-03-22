a3f63b93c8482a08c0cffc890af21c9da0f020af
xVulpeSx <jeremi.lisek@gmail.com>
2020-03-22 16:00:37 +0100

    Mapper

--- src/main/java/com/musicapp/happi/apiConsumers/ArtistMapper.java
+++ src/main/java/com/musicapp/happi/apiConsumers/ArtistMapper.java
@@ -1,7 +1,11 @@
 package com.musicapp.happi.apiConsumers;
 
+import com.musicapp.happi.apiConsumers.happi.model.responseClass.Halbum;
 import com.musicapp.happi.apiConsumers.happi.model.responseClass.Hartist;
+import com.musicapp.happi.apiConsumers.happi.model.responseClass.Htrack;
+import com.musicapp.happi.dataBase.model.Album;
 import com.musicapp.happi.dataBase.model.Artist;
+import com.musicapp.happi.dataBase.model.Track;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -13,9 +17,14 @@ public interface ArtistMapper {
 
     @Mapping(source = "artist", target = "artistName")
     @Mapping(source = "cover", target = "artistImg")
-    @Mapping(source = "Halbum.album", target = "album.albumName")
-    @Mapping(source = "Halbum.cover", target = "album.albumImg")
-    @Mapping(source = "Halbum.realease", target = "album.realeaseData")
-    @Mapping(source= "Htrack.track", target = "track.trackName")
+
     Artist hartistToArtist(Hartist hartist);
+
+    @Mapping(source = "album", target = "albumName")
+    @Mapping(source = "cover", target = "albumImg")
+    @Mapping(source = "realease", target = "realeaseDate")
+    Album halbumToAlbum(Halbum halbum);
+
+    @Mapping(source= "track", target = "trackName")
+    Track htackToTrack(Htrack htrack);
 }
